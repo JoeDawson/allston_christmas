6e5a5d3b60d5d4cfd983ccc2cdfee50299c2c8a3
David Putney <david@davidputney.com>
2016-03-19 08:33:29 -0400

    Creates js file for street fighter level

---
+++ source/scripts/functions_street.js
@@ -0,0 +1,17 @@
+
+//  this is a test
+
+var pageFunctionsStreet = {
+    intialize: function () {
+      console.log('works');
+      var self=this;
+      this.intializeWatchers(); //listens for clicks
+    },
+    intializeWatchers: function () {
+    },
+    initializeGame: function() {
+      console.log('game');
+
+
+    }
+  };
