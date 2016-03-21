603786a46df8a44b9a5f0e114d91e6902853e0b2
David Putney <david@davidputney.com>
2016-03-20 21:32:06 -0400

    Listens for keypresses and routes them to the correct place.

--- source/scripts/functions_street.js
+++ source/scripts/functions_street.js
@@ -11,7 +11,36 @@ var pageFunctionsStreet = {
     },
     initializeGame: function() {
       console.log('game');
+    handleKeyboardInput: function () {
+      console.log('keys');
 
+        var self=this;
+        document.onkeydown = function(e) {
+          var keyPress=e.keyCode
+            ? event.keyCode
+            : event.charCode;
 
-    }
+        console.log(keyPress);
+
+        var shiftKey = e.shiftKey;
+        var commandKey = event.metaKey;
+
+        if (keyPress === 37) {
+          self.handleMove('left', shiftKey, commandKey);
+        }
+        if (keyPress === 39) {
+          self.handleMove('right', shiftKey, commandKey);
+        }
+        // if (keyPress === 38) {
+        //   self.handleMove('up', shiftKey, commandKey);
+        // }
+        // if (keyPress === 40) {
+        //   self.handleMove('down', shiftKey, commandKey);
+        // }
+        if (keyPress === 32) {
+          console.log('space');
+          self.handlePunch();
+        }
+      }
+    },
   };
