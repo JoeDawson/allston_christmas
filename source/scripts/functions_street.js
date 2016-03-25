6fb1a474aec3a4e420a4f232c8a236c07e1d5e8d
David Putney <david@davidputney.com>
2016-03-25 08:29:53 -0400

    Gets an array as an argument, returns ruffled array.

--- source/scripts/functions_street.js
+++ source/scripts/functions_street.js
@@ -376,4 +376,15 @@ var pageFunctionsStreet = {
       return rando === 1;
 
     },
+    shuffle: function(input) {
+      for (var i = input.length-1; i >=0; i--) {
+
+          var randomIndex = Math.floor(Math.random()*(i+1));
+          var itemAtIndex = input[randomIndex];
+
+          input[randomIndex] = input[i];
+          input[i] = itemAtIndex;
+      }
+      return input;
+    }
   };
